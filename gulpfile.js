34e2224c643306a606dd2673cb9201e4083af982
Chris Grooms <cgrooms21@aol.com>
2019-04-10 13:54:55 -0500

    added mailgun lib

--- gulpfile.js
+++ gulpfile.js
@@ -12,84 +12,84 @@ gulp.task('default', ['watch']);
 gulp.task('build-css', function(){
   //Create an unminified version
   var full = gulp.src([
-    'src/scss/main.scss'
+    'public/src/scss/main.scss'
   ])
   . pipe(scss())
   . pipe(concat('main.css'))
-  . pipe(gulp.dest('dist/css'));
+  . pipe(gulp.dest('public/dist/css'));
 
   //Create a minified version
   var min = gulp.src([
-    'src/scss/main.scss'
+    'public/src/scss/main.scss'
   ])
   . pipe(scss())
   . pipe(cleanCSS())
   . pipe(concat('main.min.css'))
-  . pipe(gulp.dest('dist/css'));
+  . pipe(gulp.dest('public/dist/css'));
 
   return merge(full, min);
 });
 
 gulp.task('watch', function(){
-  gulp.watch('.//src/scss/**/*.scss', ['build-css']);
-  gulp.watch('.//src/scss/**/*.scss', ['build-resume']);
-  gulp.watch('.//src/scss/**/*.scss', ['build-contact']);
-  gulp.watch('.//src/scss/**/*.scss', ['build-thanks']);
+  gulp.watch('.//public/src/scss/**/*.scss', ['build-css']);
+  gulp.watch('.//public/src/scss/**/*.scss', ['build-resume']);
+  gulp.watch('.//public/src/scss/**/*.scss', ['build-contact']);
+  gulp.watch('.//public/src/scss/**/*.scss', ['build-thanks']);
 });
 
 gulp.task('build-resume', function(){
   //Create an unminified version
   var full = gulp.src([
-    'src/scss/resume.scss'
+    'public/src/scss/resume.scss'
   ])
   . pipe(scss())
   . pipe(concat('resume.css'))
-  . pipe(gulp.dest('dist/css'));
+  . pipe(gulp.dest('public/dist/css'));
 
   //Create a minified version
   var min = gulp.src([
-    'src/scss/resume.scss'
+    'public/src/scss/resume.scss'
   ])
   . pipe(scss())
   . pipe(cleanCSS())
   . pipe(concat('resume.min.css'))
-  . pipe(gulp.dest('dist/css'));
+  . pipe(gulp.dest('public/dist/css'));
 });
 
 gulp.task('build-contact', function(){
   //Create an unminified version
   var full = gulp.src([
-    'src/scss/contact.scss'
+    'public/src/scss/contact.scss'
   ])
   . pipe(scss())
   . pipe(concat('contact.css'))
-  . pipe(gulp.dest('dist/css'));
+  . pipe(gulp.dest('public/dist/css'));
 
   //Create a minified version
   var min = gulp.src([
-    'src/scss/contact.scss'
+    'public/src/scss/contact.scss'
   ])
   . pipe(scss())
   . pipe(cleanCSS())
   . pipe(concat('contact.min.css'))
-  . pipe(gulp.dest('dist/css'));
+  . pipe(gulp.dest('public/dist/css'));
 });
 
 gulp.task('build-thanks', function(){
   //Create an unminified version
   var full = gulp.src([
-    'src/scss/thanks.scss'
+    'public/src/scss/thanks.scss'
   ])
   . pipe(scss())
   . pipe(concat('thanks.css'))
-  . pipe(gulp.dest('dist/css'));
+  . pipe(gulp.dest('public/dist/css'));
 
   //Create a minified version
   var min = gulp.src([
-    'src/scss/thanks.scss'
+    'public/src/scss/thanks.scss'
   ])
   . pipe(scss())
   . pipe(cleanCSS())
   . pipe(concat('thanks.min.css'))
-  . pipe(gulp.dest('dist/css'));
+  . pipe(gulp.dest('public/dist/css'));
 });
--- public/contact.php
+++ public/contact.php
@@ -1,110 +1,58 @@
 <?php
+//Include non-vendor files
+require '../core/About/src/Validation/Validate.php';
 
-class Validate{
+//Declare Namespaces
+use About\Validation;
 
-    public $validation = [];
-
-    public $errors = [];
-
-    private $data = [];
-
-    public function notEmpty($value){
-
-        if(!empty($value)){
-            return true;
-        }
-
-        return false;
-
-    }
-
-    public function email($value){
-
-        if(filter_var($value, FILTER_VALIDATE_EMAIL)){
-            return true;
-        }
-
-        return false;
-
-    }
-
-    public function check($data){
-
-        $this->data = $data;
-
-        foreach(array_keys($this->validation) as $fieldName){
-
-            $this->rules($fieldName);
-        }
-
-    }
-
-    public function rules($field){
-        foreach($this->validation[$field] as $rule){
-            if($this->{$rule['rule']}($this->data[$field]) === false){
-                $this->errors[$field] = $rule;
-            }
-        }
-    }
-
-    public function error($field){
-        if(!empty($this->errors[$field])){
-            return $this->errors[$field]['message'];
-        }
-
-        return false;
-    }
-
-    public function userInput($key){
-        return (!empty($this->data[$key])?$this->data[$key]:null);
-    }
-}
-
-$valid = new Validate();
-
+//Validate Declarations
+$valid = new About\Validation\Validate();
 $args = [
-  'firstName'=>FILTER_SANITIZE_STRING,
-  'lastName'=>FILTER_SANITIZE_STRING,
-  'email'=>FILTER_SANITIZE_EMAIL,
-  'comment'=>FILTER_SANITIZE_STRING,
+  'name'=>FILTER_SANITIZE_STRING,
   'subject'=>FILTER_SANITIZE_STRING,
+  'message'=>FILTER_SANITIZE_STRING,
+  'email'=>FILTER_SANITIZE_EMAIL,
 ];
-
 $input = filter_input_array(INPUT_POST, $args);
 
 if(!empty($input)){
 
-    $valid->validation = [        
-        'firstName'=>[[
+    $valid->validation = [
+        'first_name'=>[[
             'rule'=>'notEmpty',
             'message'=>'Please enter your first name'
         ]],
-        'lastName'=>[[
-          'rule'=>'notEmpty',
-          'message'=>'Please enter your last name'
+        'last_name'=>[[
+            'rule'=>'notEmpty',
+            'message'=>'Please enter your last name'
         ]],
         'email'=>[[
-          'rule'=>'email',
-          'message'=>'Please enter a valid email'
-              ],[
+                'rule'=>'email',
+                'message'=>'Please enter a valid email'
+            ],[
                 'rule'=>'notEmpty',
                 'message'=>'Please enter an email'
         ]],
-        'comment'=>[[
+        'subject'=>[[
             'rule'=>'notEmpty',
-            'message'=>'Please add a comment'
+            'message'=>'Please enter a subject'
+        ]],
+        'message'=>[[
+            'rule'=>'notEmpty',
+            'message'=>'Please add a message'
         ]],
     ];
 
-    $valid->check($input);
 
-    if(empty($valid->errors)){
-        $message = "<div class=\"message-success\">Your form has been submitted!</div>";
-        //header('Location: thanks.php');
-    }else{
-        $message = "<div class=\"message-error\">Your form has errors!</div>";
-    }
+    $valid->check($input);
 }
+
+if(empty($valid->errors) && !empty($input)){
+    $message = "<div>Success!</div>";
+}else{
+    $message = "<div>Error!</div>";
+}
+
 ?>
 
 
@@ -132,7 +80,7 @@ if(!empty($input)){
 <body>
   <div id="cv" class="instaFade">
   <form action="contact.php" method="POST">
-    <div class="quickFade">
+    <div id=thanks class="quickFade">
       Thank you for taking interest in having something creatively constructed by Chris. It will be a pleasure to serve you.
     </div>
 
@@ -141,8 +89,8 @@ if(!empty($input)){
     <form action="contact.php" method="POST">
 
     <table id="table1" height="500px width="900px">
-      <thead id="tablehead">
-        Please Fill out a Contact Form and I will get back to you as soon as possible.
+      <thead>
+        <div id="tablehead">Please Fill out a Contact Form and I will get back to you as soon as possible.</div>
       </thead>
       <tbody class="quickFade">
         <tr class="quickFade delayOne">
